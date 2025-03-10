748b7f64204c9d061b30e5d68fabe36c2e98f365
joudhajal <joudhajal1997@hotmail.com>
2025-03-10 18:03:24 +0100

    fixed bug in delete

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -11,27 +11,49 @@ POSTS = [
 data_blue_print = {'content', 'title'}
 
 @app.route('/api/posts', methods=['GET', 'POST'])
-def get_posts():
+def handle_posts():
     if request.method == 'POST':
-
         data = request.json
-        missing_data = data_blue_print.difference(set(data.keys()))
+        missing_data = data_blue_print.difference(data.keys())
 
         if missing_data:
-            return jsonify({'missing-item': key for key in missing_data}), 404
+            return jsonify({'missing-items': list(missing_data)}), 404
 
-        title = ['title']
+        title = data['title']
         content = data['content']
-        _id = len(POSTS) + 1
-        new_post  = {"id": _id, "title": title, "content": content}
+        if POSTS:
+            max_post = max(POSTS, key=lambda x: x['id'])
+            _id = max_post['id'] + 1
+        else:
+            _id = 1
+        new_post = {"id": _id, "title": title, "content": content}
         POSTS.append(new_post)
 
-        return jsonify(new_post)
-    return jsonify(POSTS)
+        return jsonify(new_post), 201
+
+    if request.method == 'GET':
+        sort_type = request.args.get('sort')
+        sort_direction = request.args.get('direction')
+
+        if not sort_type or not sort_direction:
+            return jsonify(POSTS), 200
+
+        if sort_type not in data_blue_print or sort_direction not in ['asc', 'desc']:
+            return jsonify({"message": "Invalid sort parameters"}), 400
+
+        reverse = (sort_direction == 'desc')
+        sorted_posts = sorted(POSTS, key=lambda x: x[sort_type], reverse=reverse)
+        return jsonify(sorted_posts), 200
+
+
 
 @app.route('/api/posts/<int:_id>', methods=['DELETE', 'PUT'])
 def update_db(_id):
     if request.method == 'DELETE':
+        for i in range(len(POSTS)):
+            if POSTS[i]['id'] == _id:
+                del POSTS[i]
+
         if _id <= len(POSTS):
             del POSTS[_id - 1]
             return jsonify({
@@ -63,7 +85,21 @@ def update_db(_id):
             }), 404
 
 
+@app.route('/api/posts/search', methods=['GET'])
+def search_posts():
+    search_title = request.args.get('title')
+    search_content = request.args.get('content')
+    results = [
+        post for post in POSTS
+        if (search_title.lower() in post['title'].lower() if search_title else True) and
+           (search_content.lower() in post['content'].lower() if search_content else True)
+    ]
+
+    if results:
+        return jsonify(results), 200
+    else:
+        return jsonify({"message": f"No posts found that match your queries"}), 404
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=False)
