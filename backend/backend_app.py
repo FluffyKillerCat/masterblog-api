7a4e90ece3d7de559fb6a1af32ae1dcb929ee4c4
joudhajal <joudhajal1997@hotmail.com>
2025-03-10 15:22:01 +0100

    Added update method

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -8,14 +8,14 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
-
+data_blue_print = {'content', 'title'}
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
     if request.method == 'POST':
-        data_req = {'content', 'title'}
+
         data = request.json
-        missing_data = data_req.difference(set(data.keys()))
+        missing_data = data_blue_print.difference(set(data.keys()))
 
         if missing_data:
             return jsonify({'missing-item': key for key in missing_data}), 404
@@ -29,17 +29,41 @@ def get_posts():
         return jsonify(new_post)
     return jsonify(POSTS)
 
-@app.route('/api/posts/<int:_id>', methods=['DELETE'])
-def delte_post(_id):
-    if _id <= len(POSTS):
-        del POSTS[_id - 1]
-        return jsonify({
-    "message": f"Post with id {_id} has been deleted successfully."
-}), 200
-    else:
-        return jsonify({
-            "message": f"Post with id {_id} not found"
-        }), 404
+@app.route('/api/posts/<int:_id>', methods=['DELETE', 'PUT'])
+def update_db(_id):
+    if request.method == 'DELETE':
+        if _id <= len(POSTS):
+            del POSTS[_id - 1]
+            return jsonify({
+        "message": f"Post with id {_id} has been deleted successfully."
+    }), 200
+        else:
+            return jsonify({
+                "message": f"Post with id {_id} not found"
+            }), 404
+
+    elif request.method == 'PUT':
+        if _id <= len(POSTS):
+            data = request.json
+
+
+            for key, value in data.items():
+                POSTS[_id - 1][key] = value
+
+            keys = list(data.keys())
+            message = ' and '.join([' '.join(keys[:-1]), keys[-1]]) if len(keys) > 1 else ' '.join(keys)
+
+            return jsonify({
+                "message": f" {message} for post with id {_id} has been updated successfully."
+            }), 200
+
+        else:
+            return jsonify({
+                "message": f"Post with id {_id} not found"
+            }), 404
+
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
